741fb9be10c3d4e297b04f54f18e38dd43caa1e6
David Zh <david99111@mail.ru>
2023-02-19 12:16:15 +0300

    ДЗ 8

--- src/main/java/org/future/code/homework/HomeWork8.java
+++ src/main/java/org/future/code/homework/HomeWork8.java
@@ -1,6 +1,13 @@
 package org.future.code.homework;
 
 public class HomeWork8 {
+
+    /*
+        1. Скачать себе стартер итогового проекта из репозитория https://github.com/FunckingCat/Dark-Dangeon-Game
+        2. Запустить и убедиться что все работает
+
+        В проекте используется Java17, убедитесь что она установлена в настройках проекта
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
